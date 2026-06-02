0bbf4d86fdea0ebb4839b984881af18ca0cc1dd9
Aharon Seidman <41763577+hamachshev@users.noreply.github.com>
2026-06-01 21:36:03 -0400

    Add: lowest-common-ancestor-in-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-1.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# we are looking for a node that is either = to one of the p/q or between
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root: return None
+        if (root.val <= p.val and root.val >= q.val) or (root.val >= p.val and root.val <= q.val):
+            return root
+        return self.lowestCommonAncestor(root.right, p,  q) or self.lowestCommonAncestor(root.left, p, q)
+        
